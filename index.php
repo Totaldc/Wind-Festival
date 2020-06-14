3e819bc360912b5b289fb8c1c6dc9e3513ea4706
Paulius <pauliuspk@gmail.com>
2020-06-14 11:45:47 +0300

    Events section + email form

--- index.php
+++ index.php
@@ -28,10 +28,30 @@
             <h1 class="text-white mt-2">Distance Together Event Series</h1>
             <h4 class="text-white mt-3"><strong>Dates: Aug 1-28, 2020 | Squamish, BC</strong></h4>
         </div>
-
-        
     </div>
 </section>
+<!-- end of logo section -->
+<section>
+    <div class="container">
+    <h6 class="mt-3">THE SQUAMISH ARTS COUNCIL IS EXCITED TO ANNOUNCE THE 7TH ANNUAL WIND FESTIVAL WILL CONTINUE TO TAKE PLACE FROM AUGUST 1-28, 2020.  </h6>
+        <div class="d-flex justify-content-between">
+            <div class="left col-7">
+                <h1>Distance Together Event Series</h1>
+                <h3 class="font-weight-light">From a drive-in series to virtual livestreams concerts, workshops and art installations throughout Squamish the event has been reimagined but our mandate remains the same. Our distance together event series will ensure we abide by all COVID-19 guidelines to keep our community safe and supported. With this in mind, the Squamish Arts Council has curated a diverse mix of programming for all to enjoy during this time of transition.</h3>
+            </div>
+            <div class="right col-4 text-center mt-5">
+                <div class="emailForm p-4">
+                <h2 class="mt-2">SUBSCRIBE</h2>
+                <P class="font-weight-light">Sign up to the Squamish Arts Council eNewsletter to receive updates on the event schedule.</P>
+                <form action="" class="">
+                    <input type="email" name="email" placeholder="Email address:"><span><input type="submit" placeholder="Sign Up"></span>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END OF EVENTS SECTION AND EMAIL FORM -->
 
     
 <script type="text/javascript" src="assets/js/jquery.js"></script>
