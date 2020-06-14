3699ae05a33c0b47ec15231c584b439adc549d69
Paulius <pauliuspk@gmail.com>
2020-06-14 11:28:49 +0300

    Nav + logo section

---
+++ index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="row justify-content-between p-3">
+            <span>WIND FESTIVAL FOR THE ARTS</span>
+            <nav>
+                <a href="#">VIRTUAL MARKET APPLICATION</a>
+                <a href="#">SPONSORS</a>
+            </nav>
+        </div>
+    </div>
+</header>
+<!-- end of header nav -->
+<section class="bg-mountain">
+    <div class="container d-flex justify-content-center">
+        <div class="logo mt-5 text-center">
+            <img src="assets/images/wind-festival-logo.png" alt="">
+            <h1 class="text-white mt-2">Distance Together Event Series</h1>
+            <h4 class="text-white mt-3"><strong>Dates: Aug 1-28, 2020 | Squamish, BC</strong></h4>
+        </div>
+
+        
+    </div>
+</section>
+
+    
+<script type="text/javascript" src="assets/js/jquery.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
